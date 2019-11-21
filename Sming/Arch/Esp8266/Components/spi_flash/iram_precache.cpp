53055b8d8ed287189eb090b6e878b72a39863667
mikee47 <mike@sillyhouse.net>
2019-11-21 12:26:38 +0000

    Move into spi_flash Component

--- Sming/Arch/Esp8266/Components/spi_flash/iram_precache.cpp
+++ Sming/Arch/Esp8266/Components/spi_flash/iram_precache.cpp
@@ -20,7 +20,7 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "precache.h"
+#include "iram_precache.h"
 
 /* precache()
  *  @brief pre-loads flash data into the flash cache
