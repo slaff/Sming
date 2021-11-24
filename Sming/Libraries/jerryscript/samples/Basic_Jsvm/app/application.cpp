5550660caa38bc61a775ccba43eb7aefc4039e48
mikee47 <mike@sillyhouse.net>
2021-11-24 15:23:54 +0000

    Rename `exec` as `load`, use `bool` return

--- Sming/Libraries/jerryscript/samples/Basic_Jsvm/app/application.cpp
+++ Sming/Libraries/jerryscript/samples/Basic_Jsvm/app/application.cpp
@@ -12,12 +12,12 @@ constexpr char MAIN_JS_FILE[]{"main.js.snap"};
 void startJsvm()
 {
 	// Load the snapshot file
-	if(jsVm.exec(MAIN_JS_FILE) < 0) {
+	if(!jsVm.loadFromFile(MAIN_JS_FILE)) {
 		debug_e("Failed executing the following script: %s", MAIN_JS_FILE);
 		return;
 	}
 
-	// Now you can initilize your script by calling a setup() JavaScript function
+	// Now you can initialize your script by calling a setup() JavaScript function
 	if(!jsVm.runFunction("setup")) {
 		debug_e("Failed executing the setup function.");
 	}
@@ -35,7 +35,7 @@ void init()
 
 	// Mount file system, in order to work with files
 	if(!spiffs_mount()) {
-		Serial.println("Unable to mount the JS file!");
+		Serial.println("Unable to mount the file system!");
 		return;
 	}
 
