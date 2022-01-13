bb916814519a95c0fa2c14141bab404f10224853
Slavey Karadzhov <slav@attachix.com>
2022-01-13 13:49:44 +0100

    Unitial progress - nothing even compiles yet.

---
+++ Sming/Libraries/FastLED/samples/Blink/README.rst
@@ -0,0 +1,5 @@
+Sming FastLED Example (WS2801)
+==============================
+
+Simple blink example. 
+FastLED Library for Sming: https://github.com/jfaltis/Sming-FastLED
---
+++ Sming/Libraries/FastLED/samples/Blink/app/application.cpp
@@ -0,0 +1,58 @@
+#include <SmingCore.h>
+#include <FastLED.h>
+
+#define DATA_PIN 13 // GPIO 13 (D7)
+#define CLOCK_PIN 12 // GPIO 12 (D6)
+#define COLOR_ORDER RGB
+#define NUM_LEDS 32
+
+Timer procTimer;
+bool state = true;
+CRGB leds[NUM_LEDS];
+
+void blink() {
+	for(int i = 0; i < NUM_LEDS; i++) {
+		if (state) {
+			leds[i] = CRGB::Black;
+		} else {
+			leds[i] = CRGB::Red;
+		}
+	}
+
+	FastLED.show();
+
+	state = !state;
+}
+
+void init()
+{
+      // Uncomment/edit one of the following lines for your leds arrangement.
+      FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+      // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+      
+      // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
+
+      // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+	procTimer.initializeMs(1000, blink).start();
+}
